e16d8b08b9adead3b57be574b22d3db0741c3ce1
Tom Batchelor <tom@observeinc.com>
2019-09-27 11:21:51 -0700

    Added basic Zipkin

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module car-insurance
 
 go 1.13
+
+require (
+	github.com/gorilla/mux v1.7.3
+	github.com/openzipkin/zipkin-go v0.2.2
+	github.com/prometheus/client_golang v1.1.0
+)
--- main.go
+++ main.go
@@ -3,27 +3,69 @@ package main
 import (
 	"github.com/gorilla/mux"
 	"os"
-	"fmt"
+	"log"
 	"net/http"
 	"car-insurance/controllers"
     "github.com/prometheus/client_golang/prometheus/promhttp"
+    "github.com/openzipkin/zipkin-go"
+    "github.com/openzipkin/zipkin-go/model"
+    zipkinhttp "github.com/openzipkin/zipkin-go/middleware/http"
+    reporterhttp "github.com/openzipkin/zipkin-go/reporter/http"
 )
 
+const endpointURL = "http://localhost:9411/api/v2/spans"
+
 func main() {
+	tracer, err := newTracer()
+    if err != nil {
+        log.Fatal(err)
+    }
+
 	router := mux.NewRouter()
 
 	router.Handle("/metrics", promhttp.Handler())
 
 	router.HandleFunc("/insurance/simpleQuote",  controllers.SimpleQuote).Methods("POST")
+
+	router.Use(zipkinhttp.NewServerMiddleware(
+        tracer,
+        zipkinhttp.SpanName("request")), // name for request span
+    )
+
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8000" //localhost
+	    port = "8000" //localhost
 	}
 
-	fmt.Println(port)
+	log.Println(port)
 
-	err := http.ListenAndServe(":" + port, router) //Launch the app, visit localhost:8000/api
+	err = http.ListenAndServe(":" + port, router) //Launch the app, visit localhost:8000/api
 	if err != nil {
-		fmt.Print(err)
+		log.Fatal(err)
 	}
+}
+
+func newTracer() (*zipkin.Tracer, error) {
+   // The reporter sends traces to zipkin server
+   reporter := reporterhttp.NewReporter(endpointURL)
+
+   // Local endpoint represent the local service information
+   localEndpoint := &model.Endpoint{ServiceName: "my_service", Port: 8080}
+
+   // Sampler tells you which traces are going to be sampled or not. In this case we will record 100% (1.00) of traces.
+   sampler, err := zipkin.NewCountingSampler(1)
+   if err != nil {
+      return nil, err
+   }
+
+   t, err := zipkin.NewTracer(
+      reporter,
+      zipkin.WithSampler(sampler),
+      zipkin.WithLocalEndpoint(localEndpoint),
+   )
+   if err != nil {
+      return nil, err
+   }
+
+   return t, err
 }
