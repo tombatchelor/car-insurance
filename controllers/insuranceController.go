b4b02771192cf6978ce26756f9525e1479e248d5
Tom Batchelor <tom@observeinc.com>
2019-09-27 09:43:08 -0700

    Added basic logging

--- controllers/insuranceController.go
+++ controllers/insuranceController.go
@@ -2,7 +2,7 @@ package controllers
 
 import (
 	"net/http"
-	"fmt"
+	"log"
 	"encoding/json"
 	"car-insurance/model"
 	u "car-insurance/utils"
@@ -14,10 +14,13 @@ var SimpleQuote = func(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(quoteRequest)
 	if err != nil {
 		u.Respond(w, u.Message(false, "Error while decoding request body"))
-		fmt.Println(err)
+		log.Print(err)
 		return
 	}
+	log.Print(quoteRequest)
 
 	w.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(quoteRequest.Quote())
+	response := quoteRequest.Quote()
+	log.Print(response)
+	json.NewEncoder(w).Encode(response)
 }
